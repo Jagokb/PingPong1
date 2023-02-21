2b300e99fc07c78f5153422ff28d0aeeae686743
Jago <jagobiking@gmail.com>
2023-02-21 16:43:44 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask import render_template
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+
+    return render_template('final.html')
+
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=5540, debug=True, threaded=True)
+
+
